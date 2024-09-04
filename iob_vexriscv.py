0935ff50e2cf186fbff2095fcfed8e2d8ae2843f
Artur Nóbrega <arturnobrega1999@gmail.com>
2024-09-04 02:10:48 +0100

    wip(py2): Update vexriscv to support py2hwsw

--- iob_vexriscv.py
+++ iob_vexriscv.py
@@ -1,59 +1,145 @@
-#!/usr/bin/env python3
+def setup(py_params_dict):
+    attributes_dict = {
+        "original_name": "iob_vexriscv",
+        "name": "iob_vexriscv",
+        "version": "0.1",
+        "generate_hw": False,
+        "confs": [
+            {
+                "name": "ADDR_W",
+                "type": "P",
+                "val": "32",
+                "min": "1",
+                "max": "?",
+                "descr": "description here",
+            },
+            {
+                "name": "DATA_W",
+                "type": "P",
+                "val": "32",
+                "min": "1",
+                "max": "?",
+                "descr": "description here",
+            },
+            {
+                "name": "E_BIT",
+                "type": "P",
+                "val": "67",
+                "min": "1",
+                "max": "?",
+                "descr": "description here",
+            },
+            {
+                "name": "P_BIT",
+                "type": "P",
+                "val": "66",
+                "min": "1",
+                "max": "?",
+                "descr": "description here",
+            },
+            {
+                "name": "USE_EXTMEM",
+                "type": "P",
+                "val": "0",
+                "min": "0",
+                "max": "1",
+                "descr": "Select if configured for usage with external memory.",
+            },
+        ],
+        "ports": [
+            {
+                "name": "clk_en_rst",
+                "descr": "Clock, clock enable and reset",
+                "interface": {
+                    "type": "clk_en_rst",
+                    "subtype": "slave",
+                },
+            },
+            {
+                "name": "rst",
+                "descr": "Synchronous reset",
+                "signals": [
+                    {
+                        "name": "rst",
+                        "direction": "input",
+                        "width": "1",
+                        "descr": "CPU synchronous reset",
+                    },
+                    # TODO: Deprecate boot input. Instead update reset address to 0x80000000.
+                    # {
+                    #    "name": "boot",
+                    #    "direction": "input",
+                    #    "width": "1",
+                    #    "descr": "CPU boot mode",
+                    # },
+                ],
+            },
+            # TODO: Deprecate REQ and RESP
+            # {
+            #    "name": "instruction_bus",
+            #    "descr": "Instruction bus",
+            #    "signals": [
+            #        {
+            #            "name": "ibus_req",
+            #            "direction": "output",
+            #            "n_bits": "`REQ_W",
+            #            "descr": "Instruction bus request",
+            #        },
+            #        {
+            #            "name": "ibus_resp",
+            #            "direction": "input",
+            #            "n_bits": "`RESP_W",
+            #            "descr": "Instruction bus response",
+            #        },
+            #    ],
+            # },
+            # {
+            #    "name": "data_bus",
+            #    "descr": "Data bus",
+            #    "signals": [
+            #        {
+            #            "name": "dbus_req",
+            #            "direction": "output",
+            #            "n_bits": "`REQ_W",
+            #            "descr": "Data bus request",
+            #        },
+            #        {
+            #            "name": "dbus_resp",
+            #            "direction": "input",
+            #            "n_bits": "`RESP_W",
+            #            "descr": "Data bus response",
+            #        },
+            #    ],
+            # },
+            {
+                "name": "i_bus",
+                "descr": "iob-picorv32 instruction bus",
+                "interface": {
+                    "type": "iob",
+                    "subtype": "master",
+                    "port_prefix": "ibus_",
+                    "DATA_W": "DATA_W",
+                    "ADDR_W": "ADDR_W",
+                },
+            },
+            {
+                "name": "d_bus",
+                "descr": "iob-picorv32 data bus",
+                "interface": {
+                    "type": "iob",
+                    "subtype": "master",
+                    "port_prefix": "dbus_",
+                    "DATA_W": "DATA_W",
+                    "ADDR_W": "ADDR_W",
+                },
+            },
+        ],
+        "blocks": [
+            {
+                "core_name": "iob_reg_re",
+                "instance_name": "iob_reg_re_inst",
+            },
+        ],
+    }
 
-import os
-import sys
-import shutil
-
-from iob_module import iob_module
-
-# Submodules
-from iob_reg_re import iob_reg_re
-
-class iob_vexriscv(iob_module):
-    name = "iob_vexriscv"
-    version = "V0.10"
-    flows = ""
-    setup_dir=os.path.dirname(__file__)
-
-    @classmethod
-    def _create_submodules_list(cls):
-        ''' Create submodules list with dependencies of this module
-        '''
-        super()._create_submodules_list([
-            iob_reg_re,
-        ])
-
-    @classmethod
-    def _setup_confs(cls):
-        super()._setup_confs([
-            # Macros
-
-            # Parameters
-            {'name':'ADDR_W', 'type':'P', 'val':'32', 'min':'1', 'max':'?', 'descr':'description here'},
-            {'name':'DATA_W', 'type':'P', 'val':'32', 'min':'1', 'max':'?', 'descr':'description here'},
-            {'name':'E_BIT', 'type':'P', 'val':'67', 'min':'1', 'max':'?', 'descr':'description here'},
-            {'name':'P_BIT', 'type':'P', 'val':'66', 'min':'1', 'max':'?', 'descr':'description here'},
-            {'name':'USE_EXTMEM', 'type':'P', 'val':'0', 'min':'0', 'max':'1', 'descr':'Select if configured for usage with external memory.'},
-        ])
-
-    @classmethod
-    def _setup_ios(cls):
-        cls.ios += [
-            {'name': 'general', 'descr':'General interface signals', 'ports': [
-                {'name':"clk", 'type':"I", 'n_bits':'1', 'descr':"CPU clock input"},
-                {'name':"rst", 'type':"I", 'n_bits':'1', 'descr':"CPU reset input"},
-                {'name':"boot", 'type':"I", 'n_bits':'1', 'descr':"CPU boot input"},
-            ]},
-            {'name': 'instruction_bus', 'descr':'Instruction bus', 'ports': [
-                {'name':"ibus_req", 'type':"O", 'n_bits':'`REQ_W', 'descr':"Instruction bus request"},
-                {'name':"ibus_resp", 'type':"I", 'n_bits':'`RESP_W', 'descr':"Instruction bus response"},
-            ]},
-            {'name': 'data_bus', 'descr':'Data bus', 'ports': [
-                {'name':"dbus_req", 'type':"O", 'n_bits':'`REQ_W', 'descr':"Data bus request"},
-                {'name':"dbus_resp", 'type':"I", 'n_bits':'`RESP_W', 'descr':"Data bus response"},
-            ]}
-        ]
-
-    @classmethod
-    def _setup_block_groups(cls):
-        cls.block_groups += []
+    return attributes_dict
