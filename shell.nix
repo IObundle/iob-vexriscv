f85df08e766d986a42448b2726a64ab78ef609c1
Artur Nóbrega <arturnobrega1999@gmail.com>
2024-10-04 17:11:46 +0100

    license(SPDX): Add SPDX headers with MIT license

--- shell.nix
+++ shell.nix
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2024 IObundle
+#
+# SPDX-License-Identifier: MIT
+
 { pkgs ? import <nixpkgs> {}, jdk ? "jdk11" }:
   pkgs.mkShell {
     # nativeBuildInputs is usually what you want -- tools you need to run
